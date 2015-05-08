4a6a97980e7734038749abd3c76772b2ad508413
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-05-08 12:26:54 -0700

    Add lib code

---
+++ index.js
@@ -0,0 +1,12 @@
+var path = require('path')
+
+var checkEngine = require('./lib/check_engine')
+var checkModules = require('./lib/check_modules')
+
+module.exports = {
+  validate: function validate (packageJsonPath) {
+    var packageInfo = require(packageJsonPath)
+    checkEngine(packageInfo)
+    checkModules(packageInfo, `${path.dirname(packageJsonPath)}/node_modules`)
+  }
+}

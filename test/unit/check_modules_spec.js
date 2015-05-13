b13edd708655966aaf4b73fc2bd3572e22004020
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-05-13 14:49:57 -0600

    Add tests for missing dependencies

--- test/unit/check_modules_spec.js
+++ test/unit/check_modules_spec.js
@@ -1,16 +1,39 @@
+/* global describe, it */
+
 var path = require('path')
 
 var expect = require('chai').expect
 var checkModules = require('../../lib/check_modules')
 
 describe('Checking modules', function () {
+  var fixture_path = path.resolve(__dirname, '..', 'fixtures')
+
   describe('when they are valid', function () {
     it('does not throw an exception', function () {
       var packageInfo = require('../fixtures/valid_dependencies/package.json')
-      // console.log()
       checkModules(
         packageInfo,
-        path.resolve(__dirname, '..', 'fixtures', 'valid_dependencies', 'node_modules')
+        path.resolve(fixture_path, 'valid_dependencies', 'node_modules')
+      )
+    })
+  })
+
+  describe('when dependencies are missing', function () {
+    it('throws an exception', function () {
+      var packageInfo = require('../fixtures/missing_dependencies/package.json')
+      var error
+      try {
+        checkModules(
+          packageInfo,
+          path.resolve(fixture_path, 'valid_dependencies', 'node_modules')
+        )
+      } catch(e) {
+        error = e
+      }
+      expect(error).not.to.be.null
+      expect(error.name).to.eql('MissingDependenciesError')
+      expect(error.message).to.eql(
+        'Required dependencies missing: module_a, module_b. Run `npm install`'
       )
     })
   })
